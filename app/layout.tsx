f2fd0fdf91908fc3e5c263b2e06e09dcf8786465
Sobanim <sobolev.dmirtiy@gmail.com>
2025-02-23 21:45:15 +0100

    Add theme toggle functionality

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,18 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "@/assets/styles/globals.css"
-import {APP_DESCRIPTION, APP_NAME, SERVER_URL} from "@/lib/constants";
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import '@/assets/styles/globals.css';
+import { APP_DESCRIPTION, APP_NAME, SERVER_URL } from '@/lib/constants';
+import { ThemeProvider } from 'next-themes';
 
-const inter = Inter({subsets: ['latin']})
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: {
-      template: `%s | Demostore`,
-      default: APP_NAME,
+    template: `%s | Demostore`,
+    default: APP_NAME,
   },
-  description: APP_DESCRIPTION, 
-    metadataBase: new URL(SERVER_URL)
+  description: APP_DESCRIPTION,
+  metadataBase: new URL(SERVER_URL),
 };
 
 export default function RootLayout({
@@ -20,9 +21,16 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={`${inter.className} antialiased`}>
-        {children}
+    <html lang="en" suppressHydrationWarning>
+      <body className={`${inter.className} antialiasing`}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   );
