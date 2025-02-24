01deb1fbe9877f437ca8a9d9026d186fdeb47d63
Sobanim <sobolev.dmirtiy@gmail.com>
2025-02-24 21:11:00 +0100

    Add Card, ProductCard, ProductList, and ProductPrice components with sample data

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import sampleData from '@/db/sample-data';
+import ProductList from '@/components/shared/product/product-list';
 
 function Homepage() {
-    return (
-        <div>Demostore</div>
-    );
+  return (
+    <>
+      <ProductList
+        data={sampleData.products}
+        title="Newest Arrivals"
+        limit={4}
+      />
+    </>
+  );
 }
 
 export default Homepage;
