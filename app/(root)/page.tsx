795b9bb9f2ffa980219509f92d538be87adb272c
Sobanim <sobolev.dmirtiy@gmail.com>
2025-03-17 19:02:26 +0100

    Implement latest products fetching and update Homepage component

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,15 +1,12 @@
 import React from 'react';
-import sampleData from '@/db/sample-data';
 import ProductList from '@/components/shared/product/product-list';
+import { getLatestProducts } from '@/lib/actions/product.actions';
 
-function Homepage() {
+async function Homepage() {
+  const latestProducts = await getLatestProducts();
   return (
     <>
-      <ProductList
-        data={sampleData.products}
-        title="Newest Arrivals"
-        limit={4}
-      />
+      <ProductList data={latestProducts} title="Newest Arrivals" />
     </>
   );
 }
